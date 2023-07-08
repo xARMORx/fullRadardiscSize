40005aec7b44e72cc518502ebf5a40efa9e94840
ARMOR <armor875@gmail.com>
2023-07-08 19:49:47 +0300

    fix code

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,38 @@
 typedef struct { float left, top, right, bottom; } CRect; 
 typedef struct { unsigned char r, g, b, a; } CRGBA;
 
+
+void memory_fill(void* addr, const int value, const int size) {
+    DWORD newProtect;
+    VirtualProtect(addr, size, PAGE_EXECUTE_READWRITE, &newProtect);
+    memset(addr, value, size);
+    VirtualProtect(addr, size, newProtect, &newProtect);
+}
+
+template<typename T>
+T read_memory(void* addr) {
+    DWORD newProtect;
+    VirtualProtect(addr, sizeof(T), PAGE_EXECUTE_READWRITE, &newProtect);
+    T value = *reinterpret_cast<T*>(addr);
+    VirtualProtect(addr, sizeof(T), newProtect, &newProtect);
+    return value;
+}
+
 using CTimerUpdateSignature = void(__cdecl*)();
 using CRadarSpriteDrawSignature = void(__fastcall*)(void*, void*, CRect*, CRGBA*);
 
 kthook::kthook_simple<CTimerUpdateSignature> CTimerHook{};
 kthook::kthook_simple<CRadarSpriteDrawSignature> CRadarHook{};
 
-int addr_array[]{0x58A8C2, 0x58A96C, 0x58AA1A};
+int radardiscAddr[]{0x58A8C2, 0x58A96C, 0x58AA1A};
 
 void CRadar2d__DrawSprite(const decltype(CRadarHook)& hook, void* this_, void* EDX, CRect* rect, CRGBA* color){
-    rect->right *= 1.7f;
-    rect->top *= 1.135f;
+    int screenW = GetSystemMetrics(SM_CXSCREEN);
+    int screenH = GetSystemMetrics(SM_CYSCREEN);
+    float width = read_memory<float>((void*)*reinterpret_cast<uint32_t*>(0x5834C2));
+    float height = read_memory<float>((void*)*reinterpret_cast<uint32_t*>(0x58A47D));
+    rect->right += (width * (screenW * 0.00085f));
+    rect->top += (height * (screenH * 0.0012f));
     color->r = 255;
     color->g = 255;
     color->b = 255;
@@ -26,8 +47,9 @@ void CRadar2d__DrawSprite(const decltype(CRadarHook)& hook, void* this_, void* E
 void CTimer__Update(const decltype(CTimerHook)& hook){
     static bool init{};
     if (!init){
-        for (int i : addr_array){ memset((void*)(i), 0x90, 16); }
-        
+
+        for (int i : radardiscAddr){ memory_fill((void*)(i), 0x90, 16); } // nop radardisc render
+
         CRadarHook.set_dest(0x58A823);
         CRadarHook.set_cb(&CRadar2d__DrawSprite);
         CRadarHook.install();
